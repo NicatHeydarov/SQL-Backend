c5f0c3c8ba85e2246813bf196e1cd4809437e083
NIcat <nijatshh@code.edu.az>
2023-07-05 16:52:52 +0400

    first task

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+--DROP TABLE Students
+--CREATE TABLE Students
+--(
+--Id int primary key identity,
+--Name nvarchar(15)NOT NULL,
+--Surname nvarchar(20) DEFAULT'XXX',
+--Age int CHECK(Age >=18),
+--AvgPoint int
+--)
+SELECT * FROM Students
